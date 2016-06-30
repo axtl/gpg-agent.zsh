bb6200529dd23340412dd5bbbbaae154f3a03569
alexandru totolici <alexandru@fastly.com>
2016-06-29 21:55:06 -0400

    function to reset the agent

--- gpg-agent.plugin.zsh
+++ gpg-agent.plugin.zsh
@@ -1,21 +1,33 @@
 local GPG_ENV=${HOME}/.gnupg/gpg-agent.env
-if [[ ! -f "${GPG_ENV}" ]]; then
-    # start and source the script
-    eval "$(/usr/bin/env gpg-agent \
-            --quiet \
-            --daemon \
-            --enable-ssh-support \
-            --use-standard-socket \
-            --write-env-file "${GPG_ENV}" \
-            2> /dev/null)"
-    chmod 600 "${GPG_ENV}"
-fi
 
-source "${GPG_ENV}" 2> /dev/null
+function _gpg_agent_main () {
+    if [[ ! -f "${GPG_ENV}" ]]; then
+        # start and source the script
+        eval "$(/usr/bin/env gpg-agent \
+                --quiet \
+                --daemon \
+                --enable-ssh-support \
+                --use-standard-socket \
+                --write-env-file "${GPG_ENV}" \
+                2> /dev/null)"
+        chmod 600 "${GPG_ENV}"
+    fi
 
-export GPG_AGENT_INFO
-export SSH_AUTH_SOCK
-export SSH_AGENT_PID
+    source "${GPG_ENV}" 2> /dev/null
 
-GPG_TTY=$(tty)
-export GPG_TTY
+    export GPG_AGENT_INFO
+    export SSH_AUTH_SOCK
+    export SSH_AGENT_PID
+
+    GPG_TTY=$(tty)
+    export GPG_TTY
+}
+
+function _gpg_agent_reset () {
+    pkill -TERM gpg-agent
+    pkill -TERM ssh-agent
+    rm -f "${GPG_ENV}"
+    _gpg_agent_main
+}
+
+_gpg_agent_main
